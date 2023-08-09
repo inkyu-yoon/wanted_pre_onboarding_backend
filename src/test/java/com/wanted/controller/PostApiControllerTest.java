5f0f48ca2972b46c38fcf79af7dacfdc2935ceb8
inkyu-yoon <tryingmybest24h@gmail.com>
2023-08-09 12:19:40 +0900

    [test] : 게시글 조회 controller & service 테스트 코드 작성

--- src/test/java/com/wanted/controller/PostApiControllerTest.java
+++ src/test/java/com/wanted/controller/PostApiControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.domain.post.dto.PostCreateRequest;
 import com.wanted.domain.post.dto.PostCreateResponse;
+import com.wanted.domain.post.dto.PostGetResponse;
 import com.wanted.domain.user.dto.UserCreateRequest;
 import com.wanted.domain.user.dto.UserCreateResponse;
 import com.wanted.domain.user.dto.UserLoginRequest;
@@ -22,17 +23,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static com.wanted.global.exception.ErrorCode.DUPLICATE_EMAIL;
-import static com.wanted.global.exception.ErrorCode.USER_NOT_FOUND;
+import java.util.List;
+
+import static com.wanted.global.exception.ErrorCode.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -62,6 +68,9 @@ class PostApiControllerTest {
 
     PostCreateRequest postCreateRequest;
     PostCreateResponse postCreateResponse;
+    PostGetResponse postGetResponse;
+    Pageable pageable = PageRequest.of(0,20);
+
 
     @BeforeEach
     void setUp() {
@@ -79,6 +88,13 @@ class PostApiControllerTest {
         postCreateResponse = PostCreateResponse.builder()
                 .postId(postId)
                 .userId(userId)
+                .title(title)
+                .body(body)
+                .build();
+
+        postGetResponse = PostGetResponse.builder()
+                .postId(postId)
+                .author(email)
                 .title(title)
                 .body(body)
                 .build();
@@ -146,5 +162,64 @@ class PostApiControllerTest {
                     .andExpect(jsonPath("$.result").exists())
                     .andExpect(jsonPath("$.result").value("유효한 토큰이 아닙니다."));
         }
+    }
+
+    @Nested
+    @DisplayName("게시글 조회 테스트")
+    class GetPost {
+
+        @Test
+        @DisplayName("성공")
+        void getPost_success() throws Exception {
+            given(postService.getPost(postId))
+                    .willReturn(postGetResponse);
+
+            mockMvc.perform(get("/api/v1/posts/" + postId))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value("SUCCESS"))
+                    .andExpect(jsonPath("$.result").exists())
+                    .andExpect(jsonPath("$.result.postId").value(postId))
+                    .andExpect(jsonPath("$.result.author").value(email))
+                    .andExpect(jsonPath("$.result.title").value(title))
+                    .andExpect(jsonPath("$.result.body").value(body));
+        }
+
+
+        @Test
+        @DisplayName("실패 - postId에 해당하는 게시글이 존재하지 않는 경우")
+        void getPost_fail() throws Exception {
+            when(postService.getPost(postId))
+                    .thenThrow(new AppException(POST_NOT_FOUND));
+
+            mockMvc.perform(get("/api/v1/posts/" + postId))
+                    .andDo(print())
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value("ERROR"))
+                    .andExpect(jsonPath("$.result").exists())
+                    .andExpect(jsonPath("$.result").value("게시글을 찾을 수 없습니다."));
+
+        }
+
+        @Test
+        @DisplayName("성공")
+        void getPostPage_success() throws Exception {
+            given(postService.getPostPage(pageable))
+                    .willReturn(new PageImpl<>(List.of(postGetResponse)));
+
+            mockMvc.perform(get("/api/v1/posts"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value("SUCCESS"))
+                    .andExpect(jsonPath("$.result").exists())
+                    .andExpect(jsonPath("$.result.content[0].postId").value(postId))
+                    .andExpect(jsonPath("$.result.content[0].author").value(email))
+                    .andExpect(jsonPath("$.result.content[0].title").value(title))
+                    .andExpect(jsonPath("$.result.content[0].body").value(body));
+        }
+
     }
 }
