a46bb962395f0b58411c5c2dd40023eaa73631a9
inkyu-yoon <tryingmybest24h@gmail.com>
2023-08-09 15:10:21 +0900

    [style] : 주석 추가

--- src/main/java/com/wanted/service/PostService.java
+++ src/main/java/com/wanted/service/PostService.java
@@ -23,7 +23,9 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
-
+    /**
+     * 게시글 작성
+     */
     public PostCreateResponse createPost(PostCreateRequest requestDto, String email) {
         User foundUser = validateAndFindUserByEmail(email);
 
@@ -33,23 +35,33 @@ public class PostService {
     }
 
 
-
+    /**
+     * 게시글 단건 조회
+     */
+    @Transactional(readOnly = true)
     public PostGetResponse getPost(Long postId) {
         return PostGetResponse.from(validateAndFindPostById(postId));
     }
 
 
-
+    /**
+     * 게시글 페이징 단위 조회
+     */
+    @Transactional(readOnly = true)
     public Page<PostGetResponse> getPostPage(Pageable pageable) {
         return postRepository.findAll(pageable)
                 .map(post -> PostGetResponse.from(post));
     }
 
+    /**
+     * 게시글 수정
+     */
     public PostUpdateResponse updatePost(PostUpdateRequest requestDto, Long postId, String email) {
         User foundUser = validateAndFindUserByEmail(email);
 
         Post foundPost = validateAndFindPostById(postId);
 
+        // 게시글 작성자가 본인인지 확인
         if (!foundPost.checkUser(foundUser)) {
             throw new AppException(ErrorCode.USER_NOT_MATCH);
         }
@@ -59,6 +71,9 @@ public class PostService {
         return PostUpdateResponse.from(foundPost);
     }
 
+    /**
+     * 게시글 삭제
+     */
     public PostDeleteResponse deletePost(Long postId, String email) {
         User foundUser = validateAndFindUserByEmail(email);
 
@@ -73,11 +88,17 @@ public class PostService {
         return PostDeleteResponse.from(foundPost);
     }
 
+    /**
+     * 해당 Email로 가입된 회원이 있는지 검증
+     */
     private User validateAndFindUserByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
     }
 
+    /**
+     * 해당 postId로 등록된 게시글이 있는지 검증
+     */
     private Post validateAndFindPostById(Long postId) {
         return postRepository.findById(postId)
                 .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND));
